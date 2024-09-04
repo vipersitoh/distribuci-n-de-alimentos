9c4acb838ab33e40a6bc88a6a8b80636f6dcb634
vipersitoh <100100650+vipersitoh@users.noreply.github.com>
2024-09-03 22:07:07 -0400

    Update DistribucionAlimentos.java

--- DistribucionAlimentos.java
+++ DistribucionAlimentos.java
@@ -45,7 +45,7 @@ public class DistribucionAlimentos {
 
         // Mostrar los datos ingresados
         System.out.println("\nNombre del usuario: " + nombreUsuario);
-        System.out.println("\Edad del usuario: " + edadUsuario);
+        System.out.println("\nEdad del usuario: " + edadUsuario);
         System.out.println("La marca que ha ingresado es: " + marca);
         System.out.println("El modelo que ha ingresado es: " + modelo);
         System.out.println("La cilindrada que ha ingresado es: " + cilindrada);
